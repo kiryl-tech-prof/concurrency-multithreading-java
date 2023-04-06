ca7c534244f876f19b9d305f69cdfa05846519bb
Kiryl Yesipau <ky.wantshes@gmail.com>
2023-04-05 22:53:49 -0700

    Exclamation mark

--- main/src/MainSingleThread.java
+++ main/src/MainSingleThread.java
@@ -2,6 +2,6 @@ public class MainSingleThread {
 
     public static void main(String[] args) throws Exception {
         Thread.sleep(7000);
-        System.out.println("Hello, world");
+        System.out.println("Hello, world!");
     }
 }
