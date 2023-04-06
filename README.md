cf84592a7a50a7279f394b9cd5f31d9bb8840704
Kiryl Yesipau <ky.wantshes@gmail.com>
2023-04-05 22:46:44 -0700

    Main single thread class + ignore IntelliJ files

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # concurrency-multithreading-java
-TEST
+TODO
---
+++ main/src/MainSingleThread.java
@@ -0,0 +1,7 @@
+public class MainSingleThread {
+
+    public static void main(String[] args) throws Exception {
+        Thread.sleep(7000);
+        System.out.println("Hello, world");
+    }
+}
