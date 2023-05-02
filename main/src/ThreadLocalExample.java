8fb03f1d313c5be70398e99fa4be0c6db22c69b1
Kiryl Yesipau <ky.wantshes@gmail.com>
2023-05-01 21:59:26 -0700

    Simplify 5.1 Tips & Tricks

--- main/src/ThreadLocalExample.java
+++ main/src/ThreadLocalExample.java
@@ -1,33 +1,26 @@
+import java.util.UUID;
+
+
 // Example demonstrating the usage of ThreadLocal variables
 public class ThreadLocalExample {
 
-    // Announcement class that stores announcement that is different per thread
-    static class Announcement {
-        // Thread local is used to store variable per thread
-        ThreadLocal<String> announcement = new ThreadLocal<>();
-
-        // Method printing the announcement
-        void announce() {
-            System.out.println(announcement.get());
-        }
-    }
-
     // Thread that first sets the announcement and then periodically announcing that
     static class Announcer extends Thread {
-        private Announcement sharedAnnouncement;
-        private String valueToAnnounce;
 
-        Announcer(Announcement sharedAnnouncement, String valueToAnnounce) {
-            this.sharedAnnouncement = sharedAnnouncement;
-            this.valueToAnnounce = valueToAnnounce;
+        private final ThreadLocal<String> announcement;
+
+        Announcer(ThreadLocal<String> announcement) {
+            this.announcement = announcement;
         }
 
         @Override
         public void run() {
             try {
-                sharedAnnouncement.announcement.set(valueToAnnounce);
+                // First, set the value of the announcement (thread local)
+                announcement.set(UUID.randomUUID().toString());
+                // Then, periodically announce that
                 for (int i = 0; i < 10; i++) {
-                    sharedAnnouncement.announce();
+                    System.out.println(announcement.get());
                     Thread.sleep(10000);
                 }
             } catch (Exception e) {
@@ -37,9 +30,9 @@ public class ThreadLocalExample {
     }
 
     public static void main(String[] args) {
-        Announcement announcement = new Announcement();
-        new Announcer(announcement, "Pizza").start();
-        new Announcer(announcement, "Broccoli").start();
-        new Announcer(announcement, "Coffee").start();
+        ThreadLocal<String> threadLocalAnnouncement = new ThreadLocal<>();
+        new Announcer(threadLocalAnnouncement).start();
+        new Announcer(threadLocalAnnouncement).start();
+        new Announcer(threadLocalAnnouncement).start();
     }
 }
